32cb788c326a0756c59a2696acb8dd93e396bbf1
Suyash <60548103+ssuyash3000@users.noreply.github.com>
2022-06-07 03:10:15 +0530

    Create middle node of linked list.cpp

---
+++ middle node of linked list.cpp
@@ -0,0 +1,13 @@
+Node *findMiddle(Node *head) {
+    // Write your code here
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL and fast -> next != NULL){
+        slow = slow->next;
+        fast = fast -> next -> next;
+    }
+    return slow;
+    
+    
+    
+}
